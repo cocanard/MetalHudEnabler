79405c0d7601c0b04abfe7d18049033c1bd8a9f9
cocanard <>
2025-03-20 22:45:22 +0100

    Removed a mistake made in the build zsh file

--- MetalHudEnabler/Build/Build.zsh
+++ MetalHudEnabler/Build/Build.zsh
@@ -2,9 +2,9 @@
 
 dotnet publish -o ./Build/bin
 cd ./Build
-mkdir -p MetalHudEnabler.app/Contents/Ressources
+mkdir -p MetalHudEnabler.app/Contents/Resources
 cp ./Info.plist ./MetalHudEnabler.app/Contents/Info.plist
-cp ./metal.icns ./MetalHudEnabler.app/Contents/Ressources/metal.icns
+cp ./metal.icns ./MetalHudEnabler.app/Contents/Resources/metal.icns
 
 mkdir MetalHudEnabler.app/Contents/MacOS
 for file in $(find ./bin -type f); do
